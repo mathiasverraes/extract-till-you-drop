b663005dd9b0a088b8c52c9920761a85d3543ea9
Mathias Verraes <mathias@verraes.net>
2015-06-07 14:14:08 +0200

    empty test

---
+++ src/School/Tests/GroupServiceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace School\Tests;
+
+use PHPUnit_Framework_MockObject_MockObject;
+use PHPUnit_Framework_TestCase;
+use School\Group;
+use School\GroupRepository;
+use School\GroupService;
+use School\Pupil;
+use School\PupilAlreadyInGroupException;
+use School\PupilRepository;
+use School\TooManyPupilsException;
+
+final class GroupServiceTest extends PHPUnit_Framework_TestCase
+{
+
+}
+ 
